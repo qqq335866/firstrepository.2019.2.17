379ee7907a48f94e31e8c392a6db74de3b0b091a
zhangwenbo <18710546057@163.com>
2019-02-18 21:16:57 +0800

    ArrayTest update to git

---
+++ src/main/java/com/itheima/arrayandlist/testArray.java
@@ -0,0 +1,45 @@
+package com.itheima.arrayandlist;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author zwb
+ * @create 2019-01-13 11:13
+ **/
+public class testArray {
+    public static void main(String[] args) {
+        //创建一个int类型的数组
+//       int array[]={1,3,2,4,7,6,9};
+//
+//       int array1[]=new int[8];
+
+//        for (int i : array1) {
+//            System.out.println(i);
+//        }
+//
+//
+//        System.out.println(array);
+//
+//
+//        ArrayList<String> list = new ArrayList<>();
+//        List<int[]> ints = Arrays.asList(array);
+//        int[] ints1 = ints.get(0);
+//        System.out.println(ints1);
+//        //集合的长度
+//        System.out.println(ints.size());
+//        //数组进行排序
+//        Arrays.sort(array);
+//        System.out.println(array);
+//        for (int i : array) {
+//
+//            System.out.println(i);
+//        }
+        double random = Math.random();
+        System.out.println(random);
+
+
+    }
+}
